c8622ec496efb0f6c97120c2d04942548227c9d3
salimza3a <86426905+salimza3a@users.noreply.github.com>
2024-03-13 13:54:59 +0000

    Add Header component and start to design Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Layout } from "antd";
 import { Fragment } from "react/jsx-runtime";
 
+import { CustomHeader } from "./Components";
 import ErrorBoundary from "./Components/ErrorBoundary";
 import { Widget } from "./Modules/Feedback/Pages";
 
+const { Content, Footer, Header, Sider } = Layout;
 function App() {
 	return (
 		<Fragment>
 			<ErrorBoundary>
-				<Widget />
+				<Layout>
+					<CustomHeader />
+					<Widget />
+				</Layout>
 			</ErrorBoundary>
 		</Fragment>
 	);
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,5 +1,6 @@
 export { CustomButton } from "./CustomButton";
 export { CustomDivider } from "./CustomDivider";
 export { CustomDrawer } from "./CustomDrawer";
+export { CustomHeader } from "./CustomHeader";
 export { CustomIconBox } from "./CustomIconBox";
 export { CustomTabs } from "./CustomTabs";
