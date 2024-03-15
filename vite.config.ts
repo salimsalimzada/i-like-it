54a65aac2968adca70bd34824c07b44d8b087026
Salim Salimzada <salimsalimzada2002@gmail.com>
2024-03-16 02:33:34 +0400

    Add aliases for better importing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-		alias: {},
+		alias: {
+			"~": path.resolve(dirname, "./src"),
+			"~Components": path.resolve(dirname, "./src/Components"),
+			"~Modules": path.resolve(dirname, "./src/Modules"),
+		},
 	},
 });
